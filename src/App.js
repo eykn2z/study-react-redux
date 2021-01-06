64ea77e70677773efeda03c3ed276d1464f64b4c
Akane Fujii <akane.f1991@gmail.com>
2021-01-07 08:08:59 +0900

    add button using state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-function App() {
-  return (
-    <div>
-      <h1>Hellow, World!</h1>
-    </div>
-  );
+import React, { Component } from "react";
+
+const App = () => <Counter></Counter>;
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.state);
+    this.state = { count: 0 };
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <>
+        <div>counter: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </>
+    );
+  }
 }
 
 export default App;
